221a412bbf0d1b42487184b9ed55700f605f5e03
k8abadzheva <make.me.loco@gmail.com>
2024-08-20 23:04:31 +0300

    feat: add presentation level (fragments with layouts)

---
+++ app/src/main/java/com/abadzheva/composition/presentation/GameFragment.kt
@@ -0,0 +1,16 @@
+package com.abadzheva.composition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.abadzheva.composition.R
+
+class GameFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? = inflater.inflate(R.layout.fragment_game, container, false)
+}
