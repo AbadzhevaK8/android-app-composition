55c881d40f7032ef0e789e23a510b88ccaaba7e5
k8abadzheva <make.me.loco@gmail.com>
2024-08-27 23:18:40 +0300

    feat: make GameFinishedFragment logic.

--- app/src/main/java/com/abadzheva/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/abadzheva/composition/presentation/GameFinishedFragment.kt
@@ -1,5 +1,6 @@
 package com.abadzheva.composition.presentation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -33,19 +34,8 @@ class GameFinishedFragment : Fragment(R.layout.fragment_game_finished) {
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
-        val callback =
-            object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() {
-                    retryGame()
-                }
-            }
-        requireActivity().onBackPressedDispatcher.addCallback(
-            viewLifecycleOwner,
-            callback,
-        )
-        binding.buttonRetry.setOnClickListener {
-            retryGame()
-        }
+        setupClickListeners()
+        bindViews()
     }
 
     private fun parseArgs() {
@@ -53,6 +43,62 @@ class GameFinishedFragment : Fragment(R.layout.fragment_game_finished) {
             gameResult = it
         }
     }
+
+    private fun setupClickListeners() {
+        val callback =
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    retryGame()
+                }
+            }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+        binding.buttonRetry.setOnClickListener {
+            retryGame()
+        }
+    }
+
+    @SuppressLint("StringFormatMatches")
+    private fun bindViews() {
+        with(binding) {
+            emojiResult.setImageResource(getSmileResId())
+            tvRequiredAnswers.text =
+                String.format(
+                    getString(R.string.required_score),
+                    gameResult.gameSettings.minCountOfRightAnswers,
+                )
+            tvScoreAnswers.text =
+                String.format(
+                    getString(R.string.score_answers),
+                    gameResult.countOfRightAnswers,
+                )
+            tvRequiredPercentage.text =
+                String.format(
+                    getString(R.string.required_percentage),
+                    gameResult.gameSettings.minPercentOfRightAnswers,
+                )
+            tvScorePercentage.text =
+                String.format(
+                    getString(R.string.score_percentage),
+                    getPercentOfRightAnswers(),
+                )
+        }
+    }
+
+    private fun getSmileResId(): Int =
+        if (gameResult.winner) {
+            R.drawable.ic_smile
+        } else {
+            R.drawable.ic_sad
+        }
+
+    private fun getPercentOfRightAnswers() =
+        with(gameResult) {
+            if (countOfQuestions == 0) {
+                0
+            } else {
+                ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+            }
+        }
 
     private fun retryGame() {
         requireActivity()
